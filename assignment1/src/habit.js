6cc3f97a44fc30ef8cf5c886667f793847774a7d
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-01-05 16:40:03 -0500

    i finish this homework but i didn't do any styling yet

---
+++ assignment1/src/habit.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class GoodHabit extends React.Component {
+  constructor(props) {
+    super(props)
+    this.cost = "1/2 of total"
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.props.handleHabit}> Buy Good Habbits</button>
+        <ul>
+          <li>Amount: {this.props.habAmount} </li>
+          <li>Cost: {this.cost} </li>
+          <li>Rate Increase: 10%</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default GoodHabit;
