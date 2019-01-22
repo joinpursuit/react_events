8734d42897fdbac351a6bb3e1a2044f6382e747a
baynejd <jonathanbayne@pursuit.org>
2019-01-22 12:55:50 -0500

    first commit

---
+++ clicker_game/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Clicker from "./Clicker"
+
+class App extends Component {
+  render() {
+    return (
+        <>
+
+          
+          <Clicker />
+        </>
+    );
+  }
+}
+
+export default App;
